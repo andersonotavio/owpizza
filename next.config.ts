0fbb800ed5d242d1010e7d55e2684c39d79b236b
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-05-21 19:22:57 -0300

    teste deploy

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,12 @@ images: {
     remotePatterns: [
       new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/**`),
     ]
-  }
+  },
+  reactStrictMode: true,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-export const dynamic = 'force-dynamic';
 import { Header } from "@/components/layout/header";
 import { api } from "@/lib/axios";
 import { PizzaList } from "@/components/home/pizza-list";
@@ -7,14 +6,15 @@ import { Product } from "@/generated/prisma";
 
 
 export default async function Page() {
-  const pizzasReq = await api.get<{ pizzas: Product[] }>('/pizzas');
-  const pizzas = pizzasReq.data.pizzas ?? [];
+  // const pizzasReq = await api.get<{ pizzas: Product[] }>('/pizzas');
+  // const pizzas = pizzasReq.data.pizzas ?? [];
 
   return (
     <div>
       <Header />
       <main className="container mx-auto mb-10">
-        <PizzaList pizzas={pizzas} />
+        <h1>Teste</h1>
+        {/* <PizzaList pizzas={pizzas} /> */}
       </main>
     </div>
   );
